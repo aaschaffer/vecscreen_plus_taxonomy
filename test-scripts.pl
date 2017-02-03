0c42276aa04f385601005ae38f843e3099157a35
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2017-02-03 16:40:11 -0500

    updates test-scripts.pl

--- test-scripts.pl
+++ test-scripts.pl
@@ -9,26 +9,31 @@ use strict;
 select *STDOUT;
 $| = 1;
 
-##############################################################
-# Step 1: Ensure we have all input files and expected output
-#         files (to compare to actual output files we create)
-#         that we need.
-##############################################################
-printf("Checking that required input files exist ... ");
+########################################################################
+# Ensure we have all input files and expected output files (to compare
+# to actual output files we create) that we need.
+########################################################################
+# This needs to be manually updated. The code does not ensure that all 
+# input files used in the 'Test *.pl' sections below are checked for
+# in this block. So, if you add an input or expected file to a 'Test *.pl'
+# block, add it here too.
+
+my $command_width = 40;
+printf("%-*s ... ", $command_width, "Checking that required input files exist");
 my $input_dir             = "./test-files";
 my $input_root            = "test";
 my $input_dir_and_root    = $input_dir . "/" . $input_root;
-my @input_suffixes_A      = ("output_internal.txt", "output_terminal.txt");
+my @input_suffixes_A      = ("input_sequence_file.fa", "taxonomy_tree_wlevels.txt");
+#my @input_suffixes_A      = ("input_sequence_file.fa", "taxonomy_tree_wlevels.txt", "output_internal.txt", "output_terminal.txt", "combine_summaries");
 
 my $expected_dir          = "./test-files";
 my $expected_root         = "expected";
 my $expected_dir_and_root = $expected_dir . "/" . $expected_root;
-my @expected_suffixes_A   = ("output_combined.txt");
+my @expected_suffixes_A   = ("output_combined.txt", "output_combined.verbose.txt", "add_taxonomy.out", "add_taxonomy.verbose.out");
 
 my $output_dir          = "./test-files";
 my $output_root         = "tmp";
 my $output_dir_and_root = $output_dir . "/" . $output_root;
-my @output_suffixes_A   = ("output_combined.txt");
 
 my $errmsg = "";
 
@@ -44,27 +49,74 @@ foreach my $suffix (@expected_suffixes_A) {
 if($errmsg ne "") { 
   die "\n" . $errmsg;
 }
-printf("done\n");
+printf("done.\n");
 
-#####################################################################
-# Step 2: Run commands and check that output files are as expected. #
-#####################################################################
-printf("Testing combine_summaries.pl ... \n");
+###################################
+# Test from_vecscreen_to_summary.pl
+###################################
+#printf("%-*s ... ", $command_width, "Testing from_vecscreen_to_summary.pl");
+######################
+#my $cmd = "./from_vecscreen_to_summary.pl --output_root tmp --input_fasta $input_dir_and_root.input_sequence_file.fa --input_taxa input_dir_and_root.taxonomy_tree_wlevels.txt --verbose --combine_output > /dev/null"; # standard output is expected to be empty
+#run_command($cmd, 0);
+# TODO: - make from_vecscreen_to_summary.pl create and output to a directory
+#       - have from_vecscreen_to_summary.pl make sure all necessary files for all steps exist before starting step 1
+#       - update this file to test from_vecscreen_to_summary.pl
+#       - add parse_vecscreen.pl tests to this file
+#my @out_A = ("$output_dir_and_root.output_combined.txt");
+#check_many_files_exist_and_are_nonempty(\@out_A, "output");
+#diff_files("$output_dir_and_root.output_combined.txt", "$expected_dir_and_root.output_combined.txt");
+#rm_files(\@out_A);
 
+
+###########################
+# Test combine_summaries.pl
+###########################
+printf("%-*s ... ", $command_width, "Testing combine_summaries.pl");
 ######################
 my $cmd = "./combine_summaries.pl --input_terminal $input_dir_and_root.output_internal.txt --input_internal $input_dir_and_root.output_terminal.txt --outfile $output_dir_and_root.output_combined.txt > /dev/null"; # standard output is expected to be empty
 run_command($cmd, 0);
 my @out_A = ("$output_dir_and_root.output_combined.txt");
 check_many_files_exist_and_are_nonempty(\@out_A, "output");
 diff_files("$output_dir_and_root.output_combined.txt", "$expected_dir_and_root.output_combined.txt");
+rm_files(\@out_A);
+
 #####################
 # same command, but with --verbose added
 my $cmd = "./combine_summaries.pl --input_terminal $input_dir_and_root.output_internal.txt --input_internal $input_dir_and_root.output_terminal.txt --outfile $output_dir_and_root.output_combined.verbose.txt --verbose > /dev/null"; # standard output is expected to be empty
 run_command($cmd, 0);
-my @out_A = ("$output_dir_and_root.output_combined.verbose.txt");
+@out_A = ("$output_dir_and_root.output_combined.verbose.txt");
 check_many_files_exist_and_are_nonempty(\@out_A, "output");
 diff_files("$output_dir_and_root.output_combined.verbose.txt", "$expected_dir_and_root.output_combined.verbose.txt");
+rm_files(\@out_A);
+
 #####################
+printf("done.\n");
+
+######################
+# Test add_taxonomy.pl
+######################
+printf("%-*s ... ", $command_width, "Testing add_taxonomy.pl");
+######################
+my $cmd = "./add_taxonomy.pl --input_summary $input_dir_and_root.combine_summaries --input_taxa $input_dir_and_root.taxonomy_tree_wlevels.txt --outfile $output_dir_and_root.add_taxonomy.out"; 
+run_command($cmd, 0);
+@out_A = ("$output_dir_and_root.add_taxonomy.out");
+check_many_files_exist_and_are_nonempty(\@out_A, "output");
+diff_files("$output_dir_and_root.add_taxonomy.out", "$expected_dir_and_root.add_taxonomy.out");
+rm_files(\@out_A);
+
+#####################
+# same command, but with --verbose added
+my $cmd = "./add_taxonomy.pl --input_summary $input_dir_and_root.combine_summaries --input_taxa $input_dir_and_root.taxonomy_tree_wlevels.txt --outfile $output_dir_and_root.add_taxonomy.verbose.out --verbose"; 
+run_command($cmd, 0);
+@out_A = ("$output_dir_and_root.add_taxonomy.verbose.out");
+check_many_files_exist_and_are_nonempty(\@out_A, "output");
+diff_files("$output_dir_and_root.add_taxonomy.verbose.out", "$expected_dir_and_root.add_taxonomy.verbose.out");
+rm_files(\@out_A);
+
+#####################
+printf("done.\n");
+
+
 
 printf("# All tests passed.\n");
 printf("# SUCCESS\n");
@@ -186,6 +238,34 @@ sub run_command {
 
   if($? != 0) { 
     die "ERROR in $sub_name, the following command failed:\n$cmd\n";
+  }
+
+  return;
+}
+
+#################################################################
+# Subroutine:  rm_files
+# Incept:      EPN, Mon Jan 30 11:10:58 2017
+#
+# Purpose:     Remove the files listed in @{$file_AR}
+#              using unlink.
+#
+# Arguments:
+#   $file_AR:  ref to array of files to check for
+#
+# Returns:    nothing
+#
+# Dies:       Never.
+#################################################################
+sub rm_files { 
+  my $sub_name = "rm_files()";
+  my $nargs_expected = 1;
+  if(scalar(@_) != $nargs_expected) { printf STDERR ("ERROR, $sub_name entered with %d != %d input arguments.\n", scalar(@_), $nargs_expected); exit(1); } 
+
+  my ($file_AR) = @_;
+
+  foreach my $file (@{$file_AR}) { 
+    unlink $file; 
   }
 
   return;
