91753896671cddf10e60dd70504290d3be1ab620
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2017-08-09 09:30:33 -0400

    Bumps version to 0.05

--- scripts/parse_vecscreen.pl
+++ scripts/parse_vecscreen.pl
@@ -132,8 +132,8 @@ my $usage    = "Usage: perl parse_vecscreen.pl ";
 
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.04";
-my $releasedate   = "July 2017";
+my $version       = "0.05";
+my $releasedate   = "Aug 2017";
 
 # set options in %opt_HH
 opt_SetFromUserHash(\%GetOptions_H, \%opt_HH);
