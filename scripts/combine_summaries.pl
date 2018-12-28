ee931fbae8cf6b40ed37b151625c01ceb517f8f4
Alejandro A. Schaffer <schaffer@cbbdev21.be-md.ncbi.nlm.nih.gov>
2018-12-27 20:16:05 -0500

    Updated most of the vector source files in the info-files subdirectory and bumped version number to 0.16

--- scripts/combine_summaries.pl
+++ scripts/combine_summaries.pl
@@ -91,8 +91,8 @@ my $usage    = "Usage: combine_summaries.pl ";
 
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.15";
-my $releasedate   = "July 2018";
+my $version       = "0.16";
+my $releasedate   = "December 2018";
 
 # set options in %opt_HH
 opt_SetFromUserHash(\%GetOptions_H, \%opt_HH);
--- scripts/compare_vector_matches_wtaxa.pl
+++ scripts/compare_vector_matches_wtaxa.pl
@@ -211,8 +211,8 @@ my $usage    = "Usage: perl compare_vector_matches_wtaxa.pl";
 
 my $executable    = $0;
 my $date          = scalar localtime();
-my $version       = "0.15";
-my $releasedate   = "June 2018";
+my $version       = "0.16";
+my $releasedate   = "December 2018";
 
 # set options in %opt_HH
 opt_SetFromUserHash(\%GetOptions_H, \%opt_HH);
